e6c5e4eb7485396cf63e97ef3f49cde0869a7fba
Erdem Siyam <erdemsiyam@gmail.com>
2019-01-17 14:42:36 +0300

    Projenin son hali Github'a taşınmıştır.

---
+++ PageRankConsoleProject/PageRankConsoleProject/Classes/Düğüm.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PageRankConsoleProject.Classes
+{
+    public class Düğüm
+    {
+        public static List<Düğüm> dugumler = new List<Düğüm>();
+        public string isim;
+        public float hesap1 = 0;
+        public float hesap2 = 0;
+        public float pageRank = 0;
+        List<Düğüm> gidenDugumler = new List<Düğüm>();
+        List<Düğüm> gelenDugumler = new List<Düğüm>();
+
+
+
+
+        public Düğüm(string isim)
+        {
+            this.isim = isim;
+            dugumler.Add(this); // genel düğümler tablomuza eklenir.
+        }
+        public void gidenEkle(params Düğüm[] dugumler) // çokça düğüm alabilir
+        {
+            foreach (Düğüm d in dugumler) // alınan düğümler teker teker , her düğüme özel giden  ve gelen düğümler listelerine eklenir.
+            {
+                this.gidenDugumler.Add(d); // giden düğüm eklendiği gibi , giden düğüme de aşağıdaki kodda gelen eklenir.
+                d.gelenEkle(this); // gidene eklediğimiz gibi o düğümün gelenine de bu eklenir.
+            }
+
+        }
+        void gelenEkle(Düğüm d)
+        {
+            this.gelenDugumler.Add(d); // düğüm nesnesi oluşturulursa bu sınıfın statik düğümler listesine eklenir.
+        }
+
+        //hesaplamalar için static metodlar açılır , tüm düğümler için gerekli hesaplamalar yapılıp düğümlerin içindeki değerler doldurulur
+        public static void hesap_1()
+        {// hesap1 = toplam düğüm sayısı
+         //toplam düğüm sayısı hesaplanıp, her düğümün hesap1 değişkeni doldurulur
+            float toplam_dugum = 0;
+            foreach (Düğüm item in dugumler)
+            {
+                toplam_dugum++;
+            }
+            toplam_dugum = 1 / toplam_dugum;
+            foreach (Düğüm item in dugumler)
+            {
+                item.hesap1 = toplam_dugum;
+            }
+        }
+        public static void hesap_2()
+        {
+            //hesap2 = (ilgili düğümü hesap1'i  (BÖLÜ) ilgili düğüme gelen düğümleri komşuluk sayıları) * ilgili düğüme gelen her düğüm için ...  hepsinin toplamı = hesap2
+            foreach (Düğüm i1 in dugumler) // Her Düğüm için aşağıdaki işlemler yapılır
+            {
+                float hesap_2 = 0;
+                float gelen_komsunun_giden_sayisi = 0;
+
+                foreach (Düğüm i2 in i1.gelenDugumler)
+                {
+                    foreach (Düğüm i3 in i2.gidenDugumler)
+                    {
+                        gelen_komsunun_giden_sayisi++;
+                    }
+                    hesap_2 += ((i1.hesap1) / gelen_komsunun_giden_sayisi);
+                    gelen_komsunun_giden_sayisi = 0;
+                }
+                i1.hesap2 = hesap_2;
+            }
+        }
+
+        public static void hesap_3()
+        {//rangePage = ( ilgili düğüme gelen düğümlerin hesap2'si (BÖLÜ) yine o gelen düğümün komşuluk(giden) sayısı ) * tüm gelen düğümler için ... hepsinin toplamı = rangePage
+
+            foreach (Düğüm i1 in dugumler) // Her Düğüm için aşağıdaki işlemler yapılır
+            {
+                float pageRank_yeni = 0;
+                float gelen_komsunun_giden_sayisi = 0;
+
+                foreach (Düğüm i2 in i1.gelenDugumler)
+                {
+                    foreach (Düğüm i3 in i2.gidenDugumler)
+                    {
+                        gelen_komsunun_giden_sayisi++;
+                    }
+                    pageRank_yeni += ((i2.hesap2) / gelen_komsunun_giden_sayisi);
+                    gelen_komsunun_giden_sayisi = 0;
+                }
+                i1.pageRank = pageRank_yeni;
+            }
+        }
+    }
+}
---
+++ PageRankConsoleProject/PageRankConsoleProject/Program.cs
@@ -0,0 +1,60 @@
+﻿using PageRankConsoleProject.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PageRankConsoleProject
+{
+    class Program
+    {
+
+        // ÖNEMLİ : 
+        // PROGRAM C# Konsol Uygulaması olarak yazıldı.
+        // KAYNAK OLARAK BU VİDEO KULLANILMIŞTIR : https://www.youtube.com/watch?v=P8Kt6Abq_rM
+        // DEĞERLER OLDUKÇA YAKIN ÇIKMAKTADIR.
+        static void Main(string[] args)
+        {
+            // Örnek (Example) : creating nodes
+            Düğüm d1 = new Düğüm("d1");
+            Düğüm d2 = new Düğüm("d2");
+            Düğüm d3 = new Düğüm("d3");
+            Düğüm d4 = new Düğüm("d4");
+            Düğüm d5 = new Düğüm("d5");
+            Düğüm d6 = new Düğüm("d6");
+            Düğüm d7 = new Düğüm("d7");
+            Düğüm d8 = new Düğüm("d8");
+            Düğüm d9 = new Düğüm("d9");
+            Düğüm d10 = new Düğüm("d10");
+
+            // Örnek (Example) : entry the nodes connections
+            d1.gidenEkle(d6, d7, d8, d9);
+            d2.gidenEkle(d6, d7);
+            d3.gidenEkle(d6, d7);
+            d4.gidenEkle(d6, d7, d8, d9);
+            d5.gidenEkle(d6, d8, d9, d10);
+            d6.gidenEkle(d1, d2, d3, d4, d5, d7, d8, d10);
+            d7.gidenEkle(d1, d2, d3, d4, d6, d8, d9, d10);
+            d8.gidenEkle(d1, d4, d5, d6, d7, d9, d10);
+            d9.gidenEkle(d1, d4, d5, d7, d8, d10);
+            d10.gidenEkle(d5, d6, d7, d8, d9);
+
+            // Örnek (Example) : calculate the example
+            Düğüm.hesap_1();
+            Düğüm.hesap_2();
+            Düğüm.hesap_3(); //sonuncu hesap pageRank hesabı (the last one is page rank result of the example)
+
+            float pageRank_toplam = 0;
+            foreach (Düğüm item in Düğüm.dugumler) // düğüm sınıfının içinde statik tuttuğumuz tüm düğümlerin page rank'ını yazdırıyoruz.
+            {
+                Console.WriteLine(item.isim + " : " + item.pageRank.ToString());
+                pageRank_toplam += item.pageRank; // sonuçların toplamını almaktayız , 1 den farklıysa hata var demektir
+
+            }
+            Console.WriteLine("Toplam : " + pageRank_toplam.ToString()); // sonuç toplamlarını yazar.
+
+            Console.ReadLine(); // konsolu durdurmaması için beklet
+        }
+    }
+}
